c29de387e3bd50cff3c6f75440e64b1fc9d951b6
Andrea Troyano <atroyano6@gmail.com>
2026-03-24 16:04:33 +0100

    Añado carpeta de ejercicios de Js

---
+++ 010-practica-js/hello-world.js
@@ -0,0 +1,35 @@
+//console.log('Hello World');
+
+//let counter = 0;
+
+
+//console.log(counter);
+//console.log(typeof counter);
+
+//Practica 1
+//Crear una variable  myVar  y asignar el valor 0
+//Imprimir el contenido de la variable y su tipo
+let myVar = 0;
+console.log(myVar);
+console.log(typeof myVar);
+
+
+//Asignar a la misma variable el valor 4.5 y repetir paso 2
+myVar = 4.5;
+console.log(myVar);
+console.log(typeof myVar);
+
+
+//Asignar a la misma variable el valor "John Doe" y repetir paso 2
+myVar = "John Doe";
+console.log(myVar);
+console.log(typeof myVar);
+
+
+//Asignar a la misma variable el valor  false  y repetir paso 2
+myVar = false;
+console.log(myVar);
+console.log(typeof myVar);
+
+//intentar añadir la palabra  let  antes de la asignación, y luego cambiar de  let  a  const
+
